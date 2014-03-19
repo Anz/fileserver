a27a33bd3cc69af0f82c70ba127bd711d92fcbdd
Anz <knecht_roger@hotmail.com>
2014-03-19 14:09:55 +0100

    vfs refactoring

--- src/vfs.h
+++ src/vfs.h
@@ -4,24 +4,28 @@
 #include <string.h>
 #include <pthread.h>
 
-#define VFS_OPEN  0x40
 #define VFS_DEL   0x80
 #define VFS_FILE  0x01
 #define VFS_DIR   0x02
 
 typedef struct vfsn {
    pthread_rwlock_t openlk, lock;
-   char *name, *data, flags;
+   char *name, flags;
+   void *data;
    size_t data_size;
    struct vfsn *parent, *child, *sil_prev, *sil_next;
 } vfsn_t;
 
+vfsn_t* vfs_open(vfsn_t *node);
 vfsn_t* vfs_create(vfsn_t *parent, char *name, char flags);
 void vfs_delete(vfsn_t *node);
+size_t vfs_read(vfsn_t *node, void *data, size_t size);
+int vfs_write(vfsn_t *node, void *data, size_t size);
 void vfs_close(vfsn_t *node);
 void vfs_name(vfsn_t *node, char* str, size_t len);
-int vfs_file(vfsn_t* node);
-int vfs_dir(vfsn_t* node);
+int vfs_is_file(vfsn_t *node);
+int vfs_is_dir(vfsn_t *node);
+int vfs_is_deleted(vfsn_t *node);
 vfsn_t* vfs_parent(vfsn_t *node);
 vfsn_t* vfs_child(vfsn_t *node);
 vfsn_t* vfs_prev(vfsn_t *node);
