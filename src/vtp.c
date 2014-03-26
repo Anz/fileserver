658758c56058c7f0211807204336dc0ef047dc2b
Anz <knecht_roger@hotmail.com>
2014-03-26 20:47:13 +0100

    full protocol implementation

--- src/vtp.c
+++ src/vtp.c
@@ -50,7 +50,6 @@ static int vtp_read(int fd, vfsn_t *root, char *buf, size_t size)
    return 0;
 }
 
-
 static void vtp_cmd_create(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv[])
 {
    if (argc < 3) {
@@ -73,6 +72,23 @@ static void vtp_cmd_create(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* ar
    }
 
    vfs_close(node);
+}
+
+static void vtp_cmd_delete(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv[])
+{
+   if (argc < 2) {
+      write(fd, ERR_INVALIDCMD, strlen(ERR_INVALIDCMD));
+      return;
+   }
+
+   vfsn_t *file = vtp_path(cwd, argv[1]);
+   if (!file) {
+      write(fd, NOSUCHFILE, strlen(NOSUCHFILE));
+      return;
+   }
+
+   vfs_delete(file);
+   vfs_close(file);
 }
 
 static void vtp_cmd_list(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv[])
@@ -110,16 +126,45 @@ static void vtp_cmd_read(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv
    vfs_close(file);
 }
 
+static void vtp_cmd_update(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv[])
+{
+   if (argc < 3) {
+      write(fd, ERR_INVALIDCMD, strlen(ERR_INVALIDCMD));
+      return;
+   }
+
+   int len = atoi(argv[2]);
+   char content[len];
+   if (vtp_read(fd, root, content, len)) {
+      return;
+   }
+
+   vfsn_t *node = vtp_path(cwd, argv[1]);
+   if (node) {
+      vfs_write(node, content, len);
+      write(fd, MSG_FILECREATED, strlen(MSG_FILECREATED));
+   } else {
+      write(fd, ERR_FILEEXISTS, strlen(ERR_FILEEXISTS));
+   }
+
+   vfs_close(node);
+}
+
 static void vtp_cmd_exit(int fd, vfsn_t *root, vfsn_t *cwd, int argc, char* argv[])
 {
    close(fd);
 }
 
 static struct vtp_cmd cmds[] = {
+   { "ls", vtp_cmd_list },
    { "list", vtp_cmd_list },
    { "create", vtp_cmd_create },
+   { "delete", vtp_cmd_delete },
+   { "rm", vtp_cmd_delete },
    { "exit", vtp_cmd_exit },
    { "read", vtp_cmd_read },
+   { "cat", vtp_cmd_read },
+   { "update", vtp_cmd_update },
    { }
 };
 
